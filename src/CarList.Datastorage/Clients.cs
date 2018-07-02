690bc38abebd0dbe7d22ac4e0f89dcfe214a6fc8
danith <danith@mallenom.ru>
2018-07-02 17:14:07 +0300

    --

--- src/CarList.Datastorage/Clients.cs
+++ src/CarList.Datastorage/Clients.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity.ModelConfiguration;
+using Microsoft.EntityFrameworkCore;
 
 using CarList.Framework;
 
--- src/CarList.prj/Program.cs
+++ src/CarList.prj/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarList.Datastorage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace CarList
 	{
 		static void Main(string[] args)
 		{
+			var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+			var context = new Context($@"Data Source={Environment.SpecialFolder.ApplicationData}\mydb.db;Version=3;");
 		}
 	}
 }
